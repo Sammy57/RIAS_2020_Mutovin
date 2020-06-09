4d15066dae5ce46d531ff916374bdcd6475b6a81
Sammy57 <65039118+Sammy57@users.noreply.github.com>
2020-06-09 14:08:35 +0300

    Update Client.java

--- Lab1/src/Client.java
+++ Lab1/src/Client.java
@@ -15,19 +15,19 @@ public class Client{
 
             DatagramPacket dpack_send ,dpack_recv ;
 
-            Scanner inp = new Scanner(System.in);
+            Scanner inp = new Scanner(System.in); //Создание объекта in класса Scanner.
             String myName = new String();
             System.out.print("Enter name : ");
             myName = (inp.nextLine() + " : "); // здесь задается имя юзера
-            while(true){
+            while(true){ //если верно, то выполняю след:
 
-                clearBytes(arr);            // Очистить переменную с сообщением 
-                arr = (myName + inp.nextLine()).getBytes(); //ввод сообщения с консоли (+ имя которое выбрал юзер)
-                dpack_send =  new DatagramPacket( arr , arr.length ,  InetAddress.getByName("127.0.0.1") , 8080 );//делаем пакет
-                dsock.send(dpack_send);//шлем на сервер
+                clearBytes(arr);           // Очисттить переменную arr с сообщением
+                arr = (myName + inp.nextLine()).getBytes(); // имя+ ввод сообщения с консоли 
+                dpack_send =  new DatagramPacket( arr , arr.length ,  InetAddress.getByName("127.0.0.1") , 8080 ); //создаем пакет
+                dsock.send(dpack_send);//отправляем на сервер
 
-                clearBytes(arr);
-                dpack_recv = new DatagramPacket( arr , arr.length );
+                clearBytes(arr);  // Очисттить переменную arr с сообщением
+                dpack_recv = new DatagramPacket( arr , arr.length ); //Создает ячейку для содержания Датаграмных пакетов (сообщений)
                 dsock.receive(dpack_recv);
                 System.out.println(new String(arr));
 
